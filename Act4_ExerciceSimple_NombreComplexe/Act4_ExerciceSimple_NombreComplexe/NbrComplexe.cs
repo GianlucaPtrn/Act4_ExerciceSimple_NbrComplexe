3fcfa9e7a3dcd2ad4695f78e9e08e254b4452f07
GianlucaPtrn <200188@site.asty-moulin.be>
2022-11-28 11:35:05 +0100

    add files

---
+++ Act4_ExerciceSimple_NombreComplexe/Act4_ExerciceSimple_NombreComplexe/NbrComplexe.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Act4_ExerciceSimple_NombreComplexe
+{
+    class NbrComplexe
+    {
+        private double _nombreReel1;
+        private double _nombreReel2;
+
+        public double NombreReel1{
+            get{
+                return _nombreReel1; 
+            }
+            set{
+                _nombreReel1 = value; 
+            }
+        }
+
+        public double NombreReel2{
+            get{ 
+                return _nombreReel2; 
+            }
+            set{
+                _nombreReel2 = value; 
+            }
+        }
+
+        public NbrComplexe(double _nombreReel1, double _nombreReel2)
+        {
+            this._nombreReel1 = NombreReel1;
+            this._nombreReel2 = NombreReel2;
+        }
+
+        public string AfficheComplexe()
+        {
+            string complexe = $"({_nombreReel1.ToString("f2")}, {_nombreReel2.ToString("f2")})";
+            return complexe;
+
+        }
+
+        public double CalculeModule()
+        {
+            double moduleComplexe = Math.Sqrt(Math.Pow(_nombreReel1, 2) + Math.Pow(_nombreReel2, 2));
+            return moduleComplexe;
+        }
+
+        public void Ajout(double AjtReelle1, double AjtReelle2)
+        {
+            NombreReel1 += AjtReelle1;
+            NombreReel2 += AjtReelle2;
+        }
+    }
+}
---
+++ Act4_ExerciceSimple_NombreComplexe/Act4_ExerciceSimple_NombreComplexe/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Numerics;
+
+namespace Act4_ExerciceSimple_NombreComplexe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double complexeNbr1;
+            double complexeNbr2;
+            double AjtComplexeNbr1;
+            double AjtComplexeNbr2;
+            
+            Console.WriteLine("Bienvenue dans ce programme sur les complexes !");
+            Console.WriteLine("");
+
+            string reload = "oui";
+            while (reload == "oui")
+            {
+                Console.WriteLine("Que vaut la partie réelle du complexe de départ ?");
+                complexeNbr1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Que vaut la partie imaginaire du complexe de départ ?");
+                complexeNbr2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                NbrComplexe complexe = new NbrComplexe(complexeNbr1, complexeNbr2);
+
+                Console.WriteLine("Le premier complexe : " + complexe.AfficheComplexe() + " a pour module : " + complexe.CalculeModule().ToString("f2"));
+                Console.WriteLine("");
+
+
+                Console.WriteLine("Encoder un second nombre complexe : ");
+                Console.WriteLine("");
+
+                Console.WriteLine("Que vaut la partie réelle du second complexe ?");
+                AjtComplexeNbr1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Que vaut la partie imaginaire du second complexe ?");
+                AjtComplexeNbr2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                Console.WriteLine("Le second complexe est : " + $"({AjtComplexeNbr1.ToString("f2")}, {AjtComplexeNbr2.ToString("f2")})");
+                complexe.Ajout(AjtComplexeNbr1, AjtComplexeNbr2);
+                Console.WriteLine("");
+
+                Console.WriteLine("Le premier complexe devient : " + complexe.AfficheComplexe());
+                Console.WriteLine("");
+
+                Console.WriteLine("Voulez-vous rejouez ? (entrer oui)");
+                reload = Console.ReadLine();
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
